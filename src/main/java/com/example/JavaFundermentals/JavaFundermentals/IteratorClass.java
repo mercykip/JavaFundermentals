075e742b378511fc4bf6680cb9c317b32b9983b6
mercy.kipyegon <mercy.kipyegon@metropol.co.ke>
2022-04-27 09:09:55 +0300

    iteration

---
+++ src/main/java/com/example/JavaFundermentals/JavaFundermentals/IteratorClass.java
@@ -0,0 +1,25 @@
+package com.example.JavaFundermentals.JavaFundermentals;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class IteratorClass {
+    public static void main(String[] args) {
+        HashSet<String> fruits = new HashSet<String>();
+        //add
+        fruits.add("Delhi");
+        fruits.add("Mumbai");
+        fruits.add("Kolkata");
+        fruits.add("Chandigarh");
+        fruits.add("Noida");
+        // Get the iterator
+        Iterator<String> it = fruits.iterator();
+        // Print the first item
+        System.out.println(it.next());
+        //loop through a collection
+        while(it.hasNext()) {
+            System.out.print(it.next() + "");
+            System.out.println();
+        }
+    }
+}
