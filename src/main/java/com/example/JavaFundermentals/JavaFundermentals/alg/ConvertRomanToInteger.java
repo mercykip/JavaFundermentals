384ce593b7810719e3a7e52235660655982e079a
mercy <mercykip@kitktwa14l018.metropol.local>
2022-08-16 09:27:56 +0300

    Integer roman conversion

--- src/main/java/com/example/JavaFundermentals/JavaFundermentals/alg/ConvertRomanToInteger.java
+++ src/main/java/com/example/JavaFundermentals/JavaFundermentals/alg/ConvertRomanToInteger.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class ConvertRomanToInteger {
     public static int romanToInt(String s) {
         int len= s.length();
+        System.out.println("len  "+len);
         Map<Character,Integer> map=new HashMap<>();
         map.put('I',1);
         map.put('V',5);
@@ -14,12 +15,22 @@ public class ConvertRomanToInteger {
         map.put('C',100);
         map.put('D',500);
         map.put('M',1000);
-        int result=0;
-        for(int i=0; i<len; i++){
-            result+=map.get(s.charAt(i));
+        //value that corresponds to the last character of the string
+        int result=map.get(s.charAt(s.length()-1));
+        //iterate over the string starting from the second last character
+        for(int i=s.length()-2; i>0; i--){
+            System.out.println("i :: "+ map.get(s.charAt(i)));
+            System.out.println("i +1:: "+ map.get(s.charAt(i+1)));
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1))){
+                System.out.println("sub");
+                result+=map.get(s.charAt(i));
+            }else {
+                System.out.println("add");
+                result += map.get(s.charAt(i));
+            }
         }
 
-        System.out.println(result);
+//        System.out.println(result);
 
 
             return result;
