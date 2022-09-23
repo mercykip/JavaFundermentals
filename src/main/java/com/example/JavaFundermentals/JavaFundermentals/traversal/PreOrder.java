d41b7247e112bb0a1d1f3e4ee92949cbd152dbff
Mercy Jemosop <mercyjemosop@gmail.com>
2022-09-23 05:53:55 +0300

    pre-order traversal

--- src/main/java/com/example/JavaFundermentals/JavaFundermentals/traversal/PreOrder.java
+++ src/main/java/com/example/JavaFundermentals/JavaFundermentals/traversal/PreOrder.java
@@ -1,36 +1,36 @@
 package com.example.JavaFundermentals.JavaFundermentals.traversal;
-// Tree traversal in Java
 
 import com.example.JavaFundermentals.JavaFundermentals.trees.Node;
 
-public class InOrder {
-    // Root of Binary Tree
+public class PreOrder {
     Node root;
 
-    InOrder() {
-        root = null;
+    public PreOrder() {
+        this.root = null;
     }
 
     public static void main(String[] args) {
-        InOrder tree = new InOrder();
+        PreOrder tree = new PreOrder();
         tree.root = new Node(4);
         tree.root.left = new Node(2);
         tree.root.right = new Node(5);
         tree.root.left.left = new Node(1);
         tree.root.left.right = new Node(3);
         tree.root.right.right = new Node(6);
-        System.out.println("Inorder traversal");
-        tree.inorder(tree.root);
+
+        System.out.println("\nPreorder traversal ");
+        tree.preorder(tree.root);
     }
 
-    void inorder(Node node) {
+    void preorder(Node node) {
         if (node == null)
             return;
-        // Traverse left
-        inorder(node.left);
+
         // Traverse root
         System.out.print(node.data + "->");
+        // Traverse left
+        preorder(node.left);
         // Traverse right
-        inorder(node.right);
+        preorder(node.right);
     }
 }
